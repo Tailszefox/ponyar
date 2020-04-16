62954e07ddea8ef34102557257d7869a173452b7
Tailszefox <tails@tailszefox.net>
2020-04-16 04:48:59 +0200

    Moved inline JS and CSS to files

---
+++ html/js/loadswf.js
@@ -0,0 +1,12 @@
+var flashvars = {
+};
+var params = {
+    allowFullscreen: "true",
+    allowScriptAccess: "always",
+    bgcolor: "",
+    wmode: "window"
+};
+var attributes = {
+    id:"ponies"
+};
+swfobject.embedSWF("ponies.swf", "altContent", "100%", "95%", "10.3.0", "expressInstall.swf", flashvars, params, attributes);
